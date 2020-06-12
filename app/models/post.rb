b208457976b107b9b7f8710429997b1885cd5bb9
callas4ever <thetseringnorbu@gmail.com>
2020-06-12 09:06:29 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,11 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :category, inclusion: {in: %w(Fiction, Non-fiction)}
+    validates :id, presence: true
 
 
 
 
 
 
-    private
-    def be_valid
-        self.title.split().each{ |w|
-
-        }
 end
